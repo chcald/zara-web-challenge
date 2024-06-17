e10f809a58dfc81f68adfe91eaa0f9bc2f27f9da
chcald <cristianhcalderon@gmail.com>
2024-06-17 00:49:28 -0300

    add header and search input look and feel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <div className={styles.container}>
+      <header className={styles.header}>
         <Image
-          className={styles.logo}
-          src="/heart.svg"
-          alt="Heart"
-          width={24}
-          height={21.68}
+          src="/marvel-logo.svg"
+          alt="marvel logo"
+          width={130}
+          height={52}
           priority
         />
+        <div className={styles.favoritesCounterWrapper}>
+          <Image src="/heart.svg" alt="heart" width={24} height={21.68} />
+          <span>3</span>
+        </div>
+      </header>
+      <div className={styles.searchContainer}>
+        <div className={styles.searchWrapper}>
+          <FontAwesomeIcon
+            icon={faSearch}
+            className={styles.searchIcon}
+            size="sm"
+          />
+          <input
+            type="text"
+            placeholder="SEARCH A CHARACTER..."
+            className={styles.searchInput}
+          />
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
